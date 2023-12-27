8a3d0340ea8a47234a298cbe957feea067eb64c2
Davi Mattos <127062546+zollakdev@users.noreply.github.com>
2023-12-27 13:02:19 -0300

    Update login.php

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 // Dados de conexão com o MySQL
 $host = 'localhost'; // Endereço do servidor MySQL (geralmente é localhost)
 $username = 'root'; // Nome de usuário do MySQL
-$password = 'Zeylindo1@'; // Senha do MySQL
+$password = ''; // Senha do MySQL
 $database = 'formulario-davi'; // Nome do banco de dados
 
 // Iniciar a sessão
